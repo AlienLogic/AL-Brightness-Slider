10244025186f1804f1948136717ee17291833587
AlienLogic <alienvgraphic@gmail.com>
2022-03-16 21:26:06 +0100

    Created and initialized project (Visual Studio 2019)

---
+++ AL Brightness Slider/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace AL_Brightness_Slider
+{
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			MainWindow = new Views.MainWindow()
+			{
+				// can be set a DataContext, but not now
+			};
+
+			MainWindow.Show();
+		}
+	}
+}
