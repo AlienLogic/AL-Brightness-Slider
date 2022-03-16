b9b4b509d105a52d638085fe1bb1ebbfe76b836c
AlienLogic <alienvgraphic@gmail.com>
2022-03-16 21:52:46 +0100

    Added SlimShady's MonitorManagers classes, only replaced MainWindow references with System.Console

---
+++ AL Brightness Slider/MonitorManagers/WmiMonitorManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+
+namespace SlimShady.MonitorManagers
+{
+	// Tip: "WMI code creator" is really useful tool
+
+	public class WmiMonitorManager : MonitorManager
+	{
+		public static WmiMonitorManager Instance = new WmiMonitorManager();
+		private readonly List<Monitor> monitors = new List<Monitor>();
+
+		private WmiMonitorManager()
+		{
+			Console.Out.WriteLine("Loading WMI Manager");
+
+			try
+			{
+				ManagementScope scope = new ManagementScope("root\\WMI");
+				//SelectQuery query = new SelectQuery("SELECT * FROM WmiMonitorBrightness");
+				//SelectQuery query = new SelectQuery("WmiMonitorBrightness");
+				SelectQuery query = new SelectQuery("WmiMonitorBrightnessMethods");
+				ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+				// Need testers with laptops with 2 screens lol
+				foreach (var o in searcher.Get())
+				{
+					ManagementObject mObj = (ManagementObject)o;
+					monitors.Add(new WmiMonitor(mObj, this));
+				}
+			}
+			catch (ManagementException e)
+			{
+				Console.Error.WriteLine("WMI monitor Manager cannot list monitors: " + e.Message);
+			}
+		}
+
+		public override List<Monitor> GetMonitorsList()
+		{
+			return monitors;
+		}
+	}
+}
