25cd33ec0d3f58be214af920ed579d99e8a30ed7
AlienLogic <alienvgraphic@gmail.com>
2022-03-17 18:35:08 +0100

    Updated resources, added (raw) MonitorPanelItem control

---
+++ AL Brightness Slider/Views/MonitorPanelItem.xaml.cs
@@ -0,0 +1,26 @@
+﻿using SlimShady.MonitorManagers;
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AL_Brightness_Slider.Views
+{
+	/// <summary>
+	/// Logica di interazione per MonitorPanelItem.xaml
+	/// </summary>
+	public partial class MonitorPanelItem : UserControl
+{
+		Monitor monitor;
+
+		public MonitorPanelItem(Monitor monitor)
+		{
+			InitializeComponent();
+			this.monitor = monitor;
+		}
+
+		private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			monitor.Brightness = (int)e.NewValue;
+		}
+	}
+}
