76ee6c88935d216417e456e8cec63f545532158e
AlienLogic <alienvgraphic@gmail.com>
2022-03-17 22:01:56 +0100

    Method for load supported monitors, initial code for set application style (Win 7/8/10) and rought mode for change window height size

--- AL Brightness Slider/Properties/AssemblyInfo.cs
+++ AL Brightness Slider/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
 [assembly: AssemblyTitle("AL Brightness Slider")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Displays brightness control")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AL Brightness Slider")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © AlienLogic 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
--- AL Brightness Slider/Views/MainWindow.xaml.cs
+++ AL Brightness Slider/Views/MainWindow.xaml.cs
@@ -1,12 +1,61 @@
-﻿using System.Windows;
+﻿using SlimShady.MonitorManagers;
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Forms;
 
 namespace AL_Brightness_Slider.Views
 {
 	public partial class MainWindow : Window
 	{
+		private readonly List<Monitor> monitors = new List<Monitor>();
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			switch (Environment.OSVersion.Version.Major)
+			{
+				case 6:
+					switch (Environment.OSVersion.Version.Minor)
+					{
+						case 1: // Windows 7
+							break;
+						case 2: // Windows 8
+						case 3: // Windows 8.1
+							break;
+					}
+					break;
+				case 10: // Windows 10
+					ResizeMode = ResizeMode.NoResize;
+					break;
+			}
+
+			loadMonitorsPanel();
+		}
+
+		private void loadMonitorsPanel(bool reload = false)
+		{
+			if (reload)
+				monitors.Clear();
+
+			monitors.AddRange(WmiMonitorManager.Instance.GetMonitorsList());
+			monitors.AddRange(HardwareMonitorManager.Instance.GetMonitorsList());
+
+			foreach (Monitor monitor in monitors)
+				monitorsPanel.Children.Add(new MonitorPanelItem(monitor));
+		}
+
+		private void monitorsPanel_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			Height = monitorsPanel.ActualHeight;
+
+			Top = Screen.PrimaryScreen.WorkingArea.Height - Height;
+			Left = Screen.PrimaryScreen.WorkingArea.Width - Width;
 		}
 	}
 }
