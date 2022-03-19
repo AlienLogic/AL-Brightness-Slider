8731b93f401c9d4accfe788f3c34019d5d3e5f60
AlienLogic <alienvgraphic@gmail.com>
2022-03-19 22:44:55 +0100

    Improved monitor sliders

--- AL Brightness Slider/Views/MonitorPanelItem.xaml.cs
+++ AL Brightness Slider/Views/MonitorPanelItem.xaml.cs
@@ -5,8 +5,11 @@ using System.Windows.Controls;
 
 namespace AL_Brightness_Slider.Views
 {
+	// TODO: for HardwareMonitorManager change value only on drag ends
+	// https://stackoverflow.com/questions/723502/wpf-slider-with-an-event-that-triggers-after-a-user-drags
+
 	public partial class MonitorPanelItem : UserControl
-{
+	{
 		private readonly Monitor monitor;
 
 		public MonitorPanelItem(Monitor monitor)
@@ -15,9 +18,24 @@ namespace AL_Brightness_Slider.Views
 			this.monitor = monitor;
 		}
 
-		private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		private void MonitorPanelItem_Loaded(object sender, RoutedEventArgs e)
+		{
+			slider.Value = monitor.Brightness;
+			text.Content = monitor.Brightness;
+		}
+
+		private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
 		{
 			monitor.Brightness = (int)e.NewValue;
+			text.Content = monitor.Brightness;
+		}
+
+		private void slider_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+		{
+			if (e.Delta > 0)
+				slider.Value += 5;
+			else
+				slider.Value -= 5;
 		}
 	}
 }
